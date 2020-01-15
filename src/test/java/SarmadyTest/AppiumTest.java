61074276596acf21c01816dd244cd8e627857c4c
mogalal95 <m.galal.lamie@gmail.com>
2020-01-15 11:16:37 +0200

    first commit

---
+++ src/test/java/SarmadyTest/AppiumTest.java
@@ -0,0 +1,13 @@
+package SarmadyTest;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void appium()
+	{
+		System.out.println("Hello appium");
+		
+	}
+
+}
---
+++ src/test/java/SarmadyTest/SeleniumTest.java
@@ -0,0 +1,13 @@
+package SarmadyTest;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void selenium()
+	{
+		System.out.println("Hello selenium");
+		
+	}
+
+}
